237348e481ed88c858814993edc0cb341371a064
Debadrita Dey <debadritadey08@gmail.com>
2025-07-27 00:04:21 +0530

    initial

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
 import React, { useContext } from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import LoginPage from './pages/LoginPage'
+import HomePage from './pages/HomePage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
 import ProfilePage from './pages/ProfilePage'
 import { Toaster } from 'react-hot-toast'
-import { AuthContext } from '../context/AuthContext'
+import { AuthContext } from '../context/AuthContext.jsx'
 
 const App = () => {
   const { authUser } = useContext(AuthContext)
